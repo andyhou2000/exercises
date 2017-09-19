e15868ee3144ac3364cb73383ef448aa446ab662
Andy Hou <andy2khou@gmail.com>
2017-09-19 13:57:26 +0000

    jghalkfa3

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,22 +6,36 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-name = "Andy Hou"
-print(name)
+#name = "Andy Hou"
+#print(name)
 
 # display the address
-address = "360 No Scope Street"
-print(address)
+#address = "360 No Scope Street"
+#print(address)
 
 # display the city, state and zip
-cityStateZip = "Dorito City, Dewland, 13337"
-print(cityStateZip)
+#city_state_zip = "Dorito City, Dewland, 13337"
+#print(city_state_zip)
 
 # display the phone number
-phone = "360-720-1337"
-print(phone)
+#phone = "360-720-1337"
+#print(phone)
 
 # display the course area
-course = "CIS 121"
-print(course)
+#area = "CIS 121"
+#print(area)
 
+name = "Andy Hou"
+address = "360 No Scope Street"
+city_state_zip = "Dorito City, Dewland, 13337"
+phone = "360-720-1337"
+area = "CIS 121"
+
+def print_address(name,address,city_state_zip,phone,area):
+    print(name)
+    print(address)
+    print(city_state_zip)
+    print(phone)
+    print(area)
+
+print_address(name,address,city_state_zip,phone,area)
