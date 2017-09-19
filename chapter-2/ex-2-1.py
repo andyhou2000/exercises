0fd899f4f6d03b7fd9237b6501b6b43bcc5af5dd
Andy Hou <andy2khou@gmail.com>
2017-09-19 13:16:14 +0000

    lghaughsfausdifhaslidg

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Andy Hou")
 
 # display the address
-
+print("360 No Scope Street")
 
 # display the city, state and zip
-
+print("Dorito City, Dewland, 13337")
 
 # display the phone number
-
+print("360-720-1337")
 
 # display the course area
-
+print("CIS 121")
 
 
 
